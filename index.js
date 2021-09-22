f1c6b3a764db731c2f3501dc6c54335d4c58094d
Bruno Stevaux <144894s@acadiau.ca>
2021-09-22 16:34:55 -0300

    Skeleton code

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const token = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const rest = new REST({ version: '9' }).setToken(token);
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const commands = [{
+	name: 'ping',
+	description: 'Replies with Pong!'
+}]; 
+
+
+(async () => {
+	try {
+	console.log('Started refreshing application (/) commands.');
+
+	await rest.put(
+		Routes.applicationCommands(CLIENT_ID),
+	{ body: commands },
+	);
+
+	console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+	console.error(error);
+	}
+})();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+	await interaction.reply('Pong!');
+	}
+});
+
+client.login(token);
